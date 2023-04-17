c334f6ae272a2c9abbe26ba55311f4f4077fa46f
HaiYen42 <haiyentth42@gmail.com>
2023-04-17 14:39:17 +0700

    MD4-BT1

---
+++ BT1.sql
@@ -0,0 +1,12 @@
+create database student_management;
+USE student_management;
+create table Class(
+id int,
+name varchar(255)
+);
+create table Teacher (
+id int,
+name varchar(255),
+age int,
+country varchar(255)
+);
